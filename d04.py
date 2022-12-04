2cb8e4669dc2cd0ad425dea99b02ecb43b9de72e
Jens Gutow <jens_gutow@web.de>
2022-12-04 09:35:17 +0100

    read function changed

--- d04.py
+++ d04.py
@@ -1,7 +1,7 @@
 # --- Day 4: Camp Cleanup ---
 
 def read_puzzle(file):
-    return [list(map(int,s.split("-"))) for section in open(file).read().strip().split("\n") for s in section.split(",")]
+    return [[list(map(int,s.split("-"))) for s in section.split(",")] for section in open(file).read().strip().split("\n") ]
 
 def is_fully_area(p1, p2):
     check = lambda a, b: a[0] >= b[0] and a[1] <= b[1]
@@ -13,14 +13,14 @@ def is_partly_area(p1, p2):
 
 def solve(puzzle):
     c1, c2 = 0, 0
-    for i in range(len(puzzle)//2):
-        g1, g2 = puzzle[2*i], puzzle[2*i+1]
+    for g1, g2 in puzzle:
         c1 += is_fully_area(g1, g2)
         c2 += is_partly_area(g1, g2)
     return c1, c2
 
 puzzle = read_puzzle('d04.txt')
 
+print(puzzle)
 results = solve(puzzle)
 
 print(f"Task1: {results[0]}")
