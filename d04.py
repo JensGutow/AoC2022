7029d1b7829eca0511cd2a82928cbac13dc6db6a
Jens Gutow <jens_gutow@web.de>
2022-12-04 09:19:51 +0100

    day 4 - simplify

--- d04.py
+++ d04.py
@@ -12,11 +12,9 @@ def is_partly_area(p1, p2):
     return check(p1, p2) or check(p2,p1)
 
 def solve(puzzle):
-    make_group = lambda x: [x[0],x[0]] if len(x) == 1 else x
     c1, c2 = 0, 0
     for i in range(len(puzzle)//2):
-        p1,p2 = puzzle[2*i], puzzle[2*i+1]
-        g1, g2 = make_group(p1), make_group(p2)
+        g1, g2 = puzzle[2*i], puzzle[2*i+1]
         c1 += is_fully_area(g1, g2)
         c2 += is_partly_area(g1, g2)
     return c1, c2
