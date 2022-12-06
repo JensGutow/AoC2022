b2b9432069df301c12734d6bd8811c677476e603
Jens Gutow <jens_gutow@web.de>
2022-12-06 10:39:42 +0100

    Day 6 - update

--- d06.py
+++ d06.py
@@ -1,24 +1,14 @@
 # --- Day 6: Tuning Trouble ---
 
-from collections import Counter
-
 def read_puzzle(file):
     return open(file).read() 
 
 def solve1(puzzle, n):
-    cnt = Counter()
-
-    for char in puzzle:
-        cnt[char] = 1
-
     for i in range(n,len(puzzle)):
-        found = True
         if len(set(puzzle[i-n:i])) < n : continue
-        for j in range(i-n, i):
-            if cnt[puzzle[j]] > 1:
-                found = False
-        if found: break
+        break
     return i
+
 
 puzzle = read_puzzle('d06.txt')
 
